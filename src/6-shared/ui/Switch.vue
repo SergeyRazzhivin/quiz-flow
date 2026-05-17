a20d2f800dcd8ca57180da761d13823deec48263
s.razzhivin <s.razzhivin@rkeeper.ru>
2026-05-18 01:14:19 +0300

    Revert "feat(04): make toggle switches smaller"
    This reverts commit 921fd367278dbc7add13c10d616350a2fb698193.

--- src/6-shared/ui/Switch.vue
+++ src/6-shared/ui/Switch.vue
@@ -13,17 +13,8 @@ const emit = defineEmits<{
 
 <template>
   <ToggleSwitch
-    class="app-switch"
     :model-value="modelValue"
     :disabled="disabled"
     @update:model-value="emit('update:modelValue', $event)"
   />
 </template>
-
-<style scoped>
-.app-switch {
-  --p-toggleswitch-width: 2rem;
-  --p-toggleswitch-height: 1.125rem;
-  --p-toggleswitch-handle-size: 0.875rem;
-}
-</style>
